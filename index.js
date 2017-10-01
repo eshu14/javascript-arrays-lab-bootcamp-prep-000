5426ea2838cdb8f606de61f38fe1f1dcb9acba42
Easwari Ramani <eshuprad@gmail.com>
2017-10-01 18:36:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+};
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+};
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+};
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+};
+function appendKitten(name){
+
+};
+function prependKitten(name){
+
+};
+function removeLastKitten(){
+  var remLast=kittens.slice(kittens.length -1);
+  return remLast;
+};
+function removeFirstKitten(){
+  var remFirst=kittens.slice(0);
+  return remFirst;
+};
